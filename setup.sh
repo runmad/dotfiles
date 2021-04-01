29c5c8b6f25fa3704cee14de6f8fede94a8a7a06
Rune Madsen <runemadsen@mac.com>
2021-04-01 16:18:26 -0700

    Add basic setup of dotfiles

---
+++ setup.sh
@@ -0,0 +1,22 @@
+# Install Oh My Zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+
+# Install the Powerlevel10k theme
+git clone --depth=1 https://gitee.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/powerlevel10k
+ln -sf ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/powerlevel10k/powerlevel10k.zsh-theme ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/powerlevel10k.zsh-theme
+
+# Add the preferred Powerlevel10k configuration
+ln -sf ~/dotfiles/.p10k.zsh ~/.p10k.zsh
+
+# Install Oh My Zsh plugins
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
+# Remove old configuration
+mv ~/.zshrc ~/.zshrc.old
+
+# Link Zsh config
+ln -sf ~/dotfiles/.zshrc ~/.zshrc
+
+# Reload zsh config
+source ~/.zshrc
